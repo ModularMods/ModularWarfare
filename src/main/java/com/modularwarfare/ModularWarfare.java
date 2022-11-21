854ef2df2354a029a43a45a177bd32d475d06fb4
Protoxy <30320751+Protoxy22@users.noreply.github.com>
2022-11-21 16:12:41 +0100

    Added mcgltf as dependency

--- src/main/java/com/modularwarfare/ModularWarfare.java
+++ src/main/java/com/modularwarfare/ModularWarfare.java
@@ -79,7 +79,7 @@ import java.util.List;
 
 import static com.modularwarfare.common.CommonProxy.zipJar;
 
-@Mod(modid = ModularWarfare.MOD_ID, name = ModularWarfare.MOD_NAME, version = ModularWarfare.MOD_VERSION)
+@Mod(modid = ModularWarfare.MOD_ID, name = ModularWarfare.MOD_NAME, version = ModularWarfare.MOD_VERSION, dependencies = "required-client:mcgltf@[1.12.2-Forge-2.0.0.0,);")
 public class ModularWarfare {
 
     // Mod Info
